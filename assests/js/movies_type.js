9fb50476ce07da6d563ea0be7cbc7501af7cbe32
Akshay28-dev <58800770+Akshay28-dev@users.noreply.github.com>
2020-02-18 22:54:05 +0530

    Delete movies_type.js

--- assests/js/movies_type.js
+++
@@ -1,4 +0,0 @@
-//Const Variable
-const API_KEY = '?api_key=7e99decaf8f2959ca1f3df5998115397';
-const Movie_baseUrl = 'https://api.themoviedb.org/3/';
-const Movie_Img_link = 'https://image.tmdb.org/t/p/w500';
